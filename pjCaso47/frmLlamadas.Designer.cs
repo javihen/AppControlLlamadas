f1ddf7d367ee85010d2a407ce062a838770f40ba
javier <javi_hen@hotmail.com>
2020-09-28 12:48:00 -0400

    Agregar archivos de proyecto.

---
+++ pjCaso47/frmLlamadas.Designer.cs
@@ -0,0 +1,355 @@
+﻿namespace pjCaso47
+{
+    partial class frmLlamadas
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(frmLlamadas));
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.txtNumero = new System.Windows.Forms.TextBox();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.rbmInternacional = new System.Windows.Forms.RadioButton();
+            this.rbmNacional = new System.Windows.Forms.RadioButton();
+            this.rbfInternacional = new System.Windows.Forms.RadioButton();
+            this.rbfNacional = new System.Windows.Forms.RadioButton();
+            this.label3 = new System.Windows.Forms.Label();
+            this.txtMinutos = new System.Windows.Forms.TextBox();
+            this.toolStrip1 = new System.Windows.Forms.ToolStrip();
+            this.tsRegistrar = new System.Windows.Forms.ToolStripButton();
+            this.tsCancelar = new System.Windows.Forms.ToolStripButton();
+            this.toolStripSeparator1 = new System.Windows.Forms.ToolStripSeparator();
+            this.tsSalir = new System.Windows.Forms.ToolStripButton();
+            this.lvLlamadas = new System.Windows.Forms.ListView();
+            this.columnHeader1 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.columnHeader2 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.columnHeader3 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.columnHeader4 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.columnHeader5 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.columnHeader6 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.btnRegistrar = new System.Windows.Forms.Button();
+            this.btnCancelar = new System.Windows.Forms.Button();
+            this.label4 = new System.Windows.Forms.Label();
+            this.groupBox1.SuspendLayout();
+            this.toolStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Consolas", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(346, 33);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(382, 24);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "CONTROL DE REGISTRO DE LLAMADAS";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Consolas", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(9, 19);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(152, 18);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "NUMERO DE TELEFONO";
+            // 
+            // txtNumero
+            // 
+            this.txtNumero.Font = new System.Drawing.Font("Consolas", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtNumero.Location = new System.Drawing.Point(12, 35);
+            this.txtNumero.MaxLength = 15;
+            this.txtNumero.Name = "txtNumero";
+            this.txtNumero.Size = new System.Drawing.Size(149, 25);
+            this.txtNumero.TabIndex = 2;
+            this.txtNumero.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.txtNumero_KeyPress);
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.rbmInternacional);
+            this.groupBox1.Controls.Add(this.rbmNacional);
+            this.groupBox1.Controls.Add(this.rbfInternacional);
+            this.groupBox1.Controls.Add(this.rbfNacional);
+            this.groupBox1.Font = new System.Drawing.Font("Consolas", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.groupBox1.Location = new System.Drawing.Point(12, 66);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(734, 61);
+            this.groupBox1.TabIndex = 3;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "TIPO DE LLAMADA";
+            // 
+            // rbmInternacional
+            // 
+            this.rbmInternacional.AutoSize = true;
+            this.rbmInternacional.Font = new System.Drawing.Font("Consolas", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.rbmInternacional.Location = new System.Drawing.Point(550, 24);
+            this.rbmInternacional.Name = "rbmInternacional";
+            this.rbmInternacional.Size = new System.Drawing.Size(178, 22);
+            this.rbmInternacional.TabIndex = 0;
+            this.rbmInternacional.TabStop = true;
+            this.rbmInternacional.Text = "Movil Internacional";
+            this.rbmInternacional.UseVisualStyleBackColor = true;
+            // 
+            // rbmNacional
+            // 
+            this.rbmNacional.AutoSize = true;
+            this.rbmNacional.Font = new System.Drawing.Font("Consolas", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.rbmNacional.Location = new System.Drawing.Point(373, 24);
+            this.rbmNacional.Name = "rbmNacional";
+            this.rbmNacional.Size = new System.Drawing.Size(138, 22);
+            this.rbmNacional.TabIndex = 0;
+            this.rbmNacional.TabStop = true;
+            this.rbmNacional.Text = "Movil Nacional";
+            this.rbmNacional.UseVisualStyleBackColor = true;
+            // 
+            // rbfInternacional
+            // 
+            this.rbfInternacional.AutoSize = true;
+            this.rbfInternacional.Font = new System.Drawing.Font("Consolas", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.rbfInternacional.Location = new System.Drawing.Point(173, 24);
+            this.rbfInternacional.Name = "rbfInternacional";
+            this.rbfInternacional.Size = new System.Drawing.Size(170, 22);
+            this.rbfInternacional.TabIndex = 0;
+            this.rbfInternacional.TabStop = true;
+            this.rbfInternacional.Text = "Fijo Internacional";
+            this.rbfInternacional.UseVisualStyleBackColor = true;
+            // 
+            // rbfNacional
+            // 
+            this.rbfNacional.AutoSize = true;
+            this.rbfNacional.Font = new System.Drawing.Font("Consolas", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.rbfNacional.Location = new System.Drawing.Point(19, 24);
+            this.rbfNacional.Name = "rbfNacional";
+            this.rbfNacional.Size = new System.Drawing.Size(130, 22);
+            this.rbfNacional.TabIndex = 0;
+            this.rbfNacional.TabStop = true;
+            this.rbfNacional.Text = "Fijo Nacional";
+            this.rbfNacional.UseVisualStyleBackColor = true;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Consolas", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(164, 19);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(160, 18);
+            this.label3.TabIndex = 1;
+            this.label3.Text = "CANTIDAD DE MINUTOS";
+            // 
+            // txtMinutos
+            // 
+            this.txtMinutos.Font = new System.Drawing.Font("Consolas", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtMinutos.Location = new System.Drawing.Point(167, 35);
+            this.txtMinutos.Name = "txtMinutos";
+            this.txtMinutos.Size = new System.Drawing.Size(157, 25);
+            this.txtMinutos.TabIndex = 2;
+            this.txtMinutos.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.txtMinutos_KeyPress);
+            // 
+            // toolStrip1
+            // 
+            this.toolStrip1.Dock = System.Windows.Forms.DockStyle.Bottom;
+            this.toolStrip1.Font = new System.Drawing.Font("Consolas", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.toolStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.tsRegistrar,
+            this.tsCancelar,
+            this.toolStripSeparator1,
+            this.tsSalir});
+            this.toolStrip1.Location = new System.Drawing.Point(0, 381);
+            this.toolStrip1.Name = "toolStrip1";
+            this.toolStrip1.Size = new System.Drawing.Size(765, 25);
+            this.toolStrip1.TabIndex = 4;
+            this.toolStrip1.Text = "toolStrip1";
+            // 
+            // tsRegistrar
+            // 
+            this.tsRegistrar.Image = ((System.Drawing.Image)(resources.GetObject("tsRegistrar.Image")));
+            this.tsRegistrar.ImageTransparentColor = System.Drawing.Color.Magenta;
+            this.tsRegistrar.Name = "tsRegistrar";
+            this.tsRegistrar.Size = new System.Drawing.Size(164, 22);
+            this.tsRegistrar.Text = "REGISTRAR LLAMADA";
+            this.tsRegistrar.Click += new System.EventHandler(this.tsRegistrar_Click);
+            // 
+            // tsCancelar
+            // 
+            this.tsCancelar.Image = ((System.Drawing.Image)(resources.GetObject("tsCancelar.Image")));
+            this.tsCancelar.ImageTransparentColor = System.Drawing.Color.Magenta;
+            this.tsCancelar.Name = "tsCancelar";
+            this.tsCancelar.Size = new System.Drawing.Size(164, 22);
+            this.tsCancelar.Text = "CANCELAR REGISTRO";
+            this.tsCancelar.Click += new System.EventHandler(this.tsCancelar_Click);
+            // 
+            // toolStripSeparator1
+            // 
+            this.toolStripSeparator1.Name = "toolStripSeparator1";
+            this.toolStripSeparator1.Size = new System.Drawing.Size(6, 25);
+            // 
+            // tsSalir
+            // 
+            this.tsSalir.Image = ((System.Drawing.Image)(resources.GetObject("tsSalir.Image")));
+            this.tsSalir.ImageTransparentColor = System.Drawing.Color.Magenta;
+            this.tsSalir.Name = "tsSalir";
+            this.tsSalir.Size = new System.Drawing.Size(68, 22);
+            this.tsSalir.Text = "SALIR";
+            this.tsSalir.Click += new System.EventHandler(this.tsSalir_Click);
+            // 
+            // lvLlamadas
+            // 
+            this.lvLlamadas.Columns.AddRange(new System.Windows.Forms.ColumnHeader[] {
+            this.columnHeader1,
+            this.columnHeader2,
+            this.columnHeader3,
+            this.columnHeader4,
+            this.columnHeader5,
+            this.columnHeader6});
+            this.lvLlamadas.Font = new System.Drawing.Font("Consolas", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lvLlamadas.GridLines = true;
+            this.lvLlamadas.HideSelection = false;
+            this.lvLlamadas.Location = new System.Drawing.Point(12, 144);
+            this.lvLlamadas.Name = "lvLlamadas";
+            this.lvLlamadas.Size = new System.Drawing.Size(734, 175);
+            this.lvLlamadas.TabIndex = 5;
+            this.lvLlamadas.UseCompatibleStateImageBehavior = false;
+            this.lvLlamadas.View = System.Windows.Forms.View.Details;
+            // 
+            // columnHeader1
+            // 
+            this.columnHeader1.Text = "NUMERO TELF.";
+            this.columnHeader1.Width = 117;
+            // 
+            // columnHeader2
+            // 
+            this.columnHeader2.Text = "TIPO LLAMADA";
+            this.columnHeader2.Width = 190;
+            // 
+            // columnHeader3
+            // 
+            this.columnHeader3.Text = "MINUTOS";
+            this.columnHeader3.Width = 79;
+            // 
+            // columnHeader4
+            // 
+            this.columnHeader4.Text = "IMPORTE";
+            this.columnHeader4.Width = 116;
+            // 
+            // columnHeader5
+            // 
+            this.columnHeader5.Text = "DESCUENTO";
+            this.columnHeader5.Width = 112;
+            // 
+            // columnHeader6
+            // 
+            this.columnHeader6.Text = "NETO A PAGAR";
+            this.columnHeader6.Width = 115;
+            // 
+            // btnRegistrar
+            // 
+            this.btnRegistrar.Location = new System.Drawing.Point(13, 326);
+            this.btnRegistrar.Name = "btnRegistrar";
+            this.btnRegistrar.Size = new System.Drawing.Size(197, 43);
+            this.btnRegistrar.TabIndex = 6;
+            this.btnRegistrar.Text = "REGISTRAR";
+            this.btnRegistrar.UseVisualStyleBackColor = true;
+            this.btnRegistrar.Click += new System.EventHandler(this.btnRegistrar_Click);
+            // 
+            // btnCancelar
+            // 
+            this.btnCancelar.Location = new System.Drawing.Point(216, 325);
+            this.btnCancelar.Name = "btnCancelar";
+            this.btnCancelar.Size = new System.Drawing.Size(197, 43);
+            this.btnCancelar.TabIndex = 6;
+            this.btnCancelar.Text = "CANCELAR";
+            this.btnCancelar.UseVisualStyleBackColor = true;
+            this.btnCancelar.Click += new System.EventHandler(this.btnCancelar_Click);
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label4.ForeColor = System.Drawing.Color.Red;
+            this.label4.Location = new System.Drawing.Point(436, 336);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(187, 32);
+            this.label4.TabIndex = 7;
+            this.label4.Text = "Los botones y la barra inferior \r\ncumplen la misma funcion";
+            // 
+            // frmLlamadas
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(765, 406);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.btnCancelar);
+            this.Controls.Add(this.btnRegistrar);
+            this.Controls.Add(this.lvLlamadas);
+            this.Controls.Add(this.toolStrip1);
+            this.Controls.Add(this.groupBox1);
+            this.Controls.Add(this.txtMinutos);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.txtNumero);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Name = "frmLlamadas";
+            this.Text = "Control de registro de llamadas";
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.toolStrip1.ResumeLayout(false);
+            this.toolStrip1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox txtNumero;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.RadioButton rbmInternacional;
+        private System.Windows.Forms.RadioButton rbmNacional;
+        private System.Windows.Forms.RadioButton rbfInternacional;
+        private System.Windows.Forms.RadioButton rbfNacional;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox txtMinutos;
+        private System.Windows.Forms.ToolStrip toolStrip1;
+        private System.Windows.Forms.ToolStripButton tsRegistrar;
+        private System.Windows.Forms.ToolStripButton tsCancelar;
+        private System.Windows.Forms.ToolStripSeparator toolStripSeparator1;
+        private System.Windows.Forms.ToolStripButton tsSalir;
+        private System.Windows.Forms.ListView lvLlamadas;
+        private System.Windows.Forms.ColumnHeader columnHeader1;
+        private System.Windows.Forms.ColumnHeader columnHeader2;
+        private System.Windows.Forms.ColumnHeader columnHeader3;
+        private System.Windows.Forms.ColumnHeader columnHeader4;
+        private System.Windows.Forms.ColumnHeader columnHeader5;
+        private System.Windows.Forms.ColumnHeader columnHeader6;
+        private System.Windows.Forms.Button btnRegistrar;
+        private System.Windows.Forms.Button btnCancelar;
+        private System.Windows.Forms.Label label4;
+    }
+}
+
---
+++ pjCaso47/frmLlamadas.cs
@@ -0,0 +1,132 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace pjCaso47
+{
+    public partial class frmLlamadas : Form
+    {
+        public frmLlamadas()
+        {
+            InitializeComponent();
+        }
+        //Boton de registro
+        private void btnRegistrar_Click(object sender, EventArgs e)
+        {
+            tsRegistrar_Click(sender, e);
+        }
+        //Boton de cancelar
+        private void btnCancelar_Click(object sender, EventArgs e)
+        {
+            tsCancelar_Click(sender, e);
+        }
+        private void tsRegistrar_Click(object sender, EventArgs e)
+        {
+            //Capturando los datos 
+            string telefono = txtNumero.Text;
+            int minutos = int.Parse(txtMinutos.Text);
+
+            //Determinando el tipo de llamada
+            string tipo="";
+            if (rbfNacional.Checked == true) tipo = "Fijo Nacional";
+            if (rbfInternacional.Checked == true) tipo = "Fijo Internacional";
+            if (rbmNacional.Checked == true) tipo = "Movil Nacional";
+            if (rbmInternacional.Checked == true) tipo = "Movil Internacional";
+
+            //
+            if (tipo == "")
+            {
+                MessageBox.Show("Debe seleccionar el tipo de llamada..!!","Llamadas");
+                return;
+            }
+            
+
+            //Asignando una tarifa segun el tipo de llamada
+            double tarifa=0;
+            switch (tipo)
+            {
+                case "Fijo Nacional": tarifa = 0.25; break;
+                case "Fijo Internacional": tarifa = 1.75; break;
+                case "Movil Nacional": tarifa = 1.25; break;
+                case "Movil Internacional": tarifa = 2.50; break;
+            }
+
+            //Realizando los calculos de los importes
+            double importe = tarifa * minutos;
+
+            //Determinando el descuento segun los minutos
+            double descuento;
+            switch (tipo)
+            {
+                case "Fijo Nacional": descuento = 5.0/100 * importe; break;
+                case "Fijo Internacional": descuento = 7.0 / 100 * importe; break;
+                case "Movil Nacional": descuento = 9.0 / 100 * importe; break;
+                case "Movil Internacional": descuento = 12.0 / 100 * importe; break;
+                default: descuento = 0; break;
+            }
+
+            //Calculando el neto 
+            double neto = importe - descuento;
+
+            //Realizando impresiones
+            ListViewItem fila = new ListViewItem(telefono);
+            fila.SubItems.Add(tipo);
+            fila.SubItems.Add(minutos.ToString());
+            fila.SubItems.Add(importe.ToString("C"));
+            fila.SubItems.Add(descuento.ToString("C"));
+            fila.SubItems.Add(neto.ToString("C"));
+            lvLlamadas.Items.Add(fila);
+
+            //Limpiando los controles
+            tsCancelar_Click(sender,e);
+        }
+
+        private void tsCancelar_Click(object sender, EventArgs e)
+        {
+            txtNumero.Clear();
+            txtMinutos.Clear();
+            rbfNacional.Checked = false;
+            rbfInternacional.Checked = false;
+            rbmNacional.Checked = false;
+            rbmInternacional.Checked = false;
+            txtNumero.Focus();
+        }
+
+        private void tsSalir_Click(object sender, EventArgs e)
+        {
+            DialogResult r = MessageBox.Show("Esta seguro de salir?",
+                                              "Llamadas",
+                                              MessageBoxButtons.YesNo,
+                                              MessageBoxIcon.Exclamation);
+            if (r == DialogResult.Yes) this.Close();
+        }
+        //Metodo para que solamente ingrese numeros en el cuadro de texto
+        private void txtMinutos_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            if (!(char.IsNumber(e.KeyChar)) && (e.KeyChar != (char)Keys.Back))
+            {
+                MessageBox.Show("Debe ingresar solo numeros","Llamadas");
+                e.Handled = true;
+                return;
+            }
+        }
+        //Metodo para que solamente ingrese numeros en el cuadro de texto
+        private void txtNumero_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            if (!(char.IsNumber(e.KeyChar)) && (e.KeyChar != (char)Keys.Back))
+            {
+                MessageBox.Show("Debe ingresar solo numeros", "Llamadas");
+                e.Handled = true;
+                return;
+            }
+        }
+
+        
+    }
+}
